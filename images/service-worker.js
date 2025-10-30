94ed35a19af66affa34de2d89e04aa67f583410b
healingosus <kssapnupuas@gmail.com>
2025-10-30 20:53:24 +0800

    Add files via upload

---
+++ images/service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/A004%20KARL%20IVERSON%20SERRANO.HTML',  // Add other files you need to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
